f182c66dc7b1db6e5709c2a13ee2cdfa4d48813d
necfol <necfoll@gmail.com>
2017-07-19 22:25:24 +0800

    initial project

---
+++ CLearn-0/CLearn-0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CLearn-0
+//
+//  Created by necfol on 2017/7/19.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
