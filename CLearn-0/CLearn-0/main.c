c4a6349fd35c8043b191a4f2ef464c3d7e16fcc7
necfol <necfoll@gmail.com>
2017-07-20 16:18:59 +0800

    pointer

--- CLearn-0/CLearn-0/main.c
+++ CLearn-0/CLearn-0/main.c
@@ -10,6 +10,13 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    char a;
+//    定义一个指针变量b，并且b只能指向char类型的数据
+    char *b;
+//    指针变量的值为a的地址
+    b = &a;
+    
+//    访问b值对应的存储空间，即变量a的存储空间，此时的*和上面的* 是不同的
+    *b = 10;
     return 0;
 }
